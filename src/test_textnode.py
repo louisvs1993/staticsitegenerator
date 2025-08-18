e5b7b18c38f5ceb4cc864f50ba66f5a86fd6f8e3
louisvs1993 <louisvansteelant@hotmail.com>
2025-08-18 19:18:13 +0200

    changes to textnode and tests added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -13,12 +13,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node.__repr__(), "TextNode(This is a text node, TextType.BOLD, None)")
 
-    def test_text_type(self):
+    def test_text_type_notequal(self):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-    def test_text(self):
+    def test_text_notequal(self):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node but different", TextType.TEXT)
         self.assertNotEqual(node, node2)
@@ -31,6 +31,45 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("text", TextType.TEXT, url=None)
         node2 = TextNode("text", TextType.TEXT, url="http://example.com")
         self.assertNotEqual(node1, node2)
+
+    def test_image(self):
+        node = TextNode("A bear", TextType.IMAGE, "https://boot.dev/bear.png")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://boot.dev/bear.png")
+        self.assertEqual(html_node.props["alt"], "A bear")
+    
+    def test_link(self):
+        node = TextNode("A bear", TextType.LINK, "https://boot.dev/bear.png")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "A bear")
+        self.assertEqual(html_node.props["href"], "https://boot.dev/bear.png")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
         
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -22,3 +23,25 @@ class TextNode:
         
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html_node(self):
+        if self.text_type not in TextType:
+            raise Exception("Type not correct")
+        elif self.text_type == TextType.TEXT:
+            return LeafNode(None, self.text)
+        elif self.text_type == TextType.BOLD:
+            return LeafNode("b", self.text)
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode("i", self.text)
+        elif self.text_type == TextType.CODE:
+            return LeafNode("code", self.text)
+        elif self.text_type == TextType.LINK:
+            if self.url != None:
+                return LeafNode("a", self.text, {"href":self.url})
+            else:
+                raise Exception("Url required")
+        elif self.text_type == TextType.IMAGE:
+            if self.url != None:
+                return LeafNode("img", "", {"src":self.url,"alt":self.text})
+            else:
+                raise Exception("Url required")
