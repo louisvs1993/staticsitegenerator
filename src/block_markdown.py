e95b21f3922db9f59d1ce32754f44dd0599da3a9
louisvs1993 <louisvansteelant@hotmail.com>
2025-08-25 16:00:16 +0200

    site generation + tests added

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -13,7 +13,7 @@ class BlockType(Enum):
     QUOTE = "quote"
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
-
+    
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
@@ -17,5 +19,12 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 main()
